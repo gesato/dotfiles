2ce66913644064c1745ee4bbf8f380e4216a6a77
Genki Sato Mac <satogenki1115@gmail.com>
2014-11-24 20:13:57 +0900

    update peco setting

--- setup.sh
+++ setup.sh
@@ -7,3 +7,4 @@ ln -s ~/dotfiles/.gemrc ~/.gemrc
 ln -s ~/dotfiles/.git-completion.bash ~/.git-completion.bash
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.tmuxinator ~/.tmuxinator
+ln -s ~/dotfiles/.peco ~/
