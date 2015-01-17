0992c4bdbf29c9c21f62f4bc9b9722d294f95502
Genki Sato Mac <satogenki1115@gmail.com>
2015-01-17 09:57:00 +0900

    add mysql setting

--- setup.sh
+++ setup.sh
@@ -7,4 +7,6 @@ ln -s ~/dotfiles/.gemrc ~/.gemrc
 ln -s ~/dotfiles/.git-completion.bash ~/.git-completion.bash
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.tmuxinator ~/.tmuxinator
+ln -s ~/dotfiles/.my.cnf  ~/.my.cnf
+ln -s ~/dotfiles/.grcat  ~/.grcat
 ln -s ~/dotfiles/.peco ~/
